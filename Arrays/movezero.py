cfb228dfc47db5a5a04dc14c8e9317c379a38fd3
undocument <undocument2024@gmail.com>
2025-02-24 20:18:21 -0800

    on date 02/24

--- Arrays/movezero.py
+++ Arrays/movezero.py
@@ -1,9 +1,14 @@
 arr = [2,0,4,5,6,0]
 
 def movezero(a):
+    last_noonzero_index = 0
 
-   for i in range(len(a)):
-       if a[i] == 0:
-           print(a[i])
+    for i in range(len(a)):
+        if a[i] !=0:
+            print(a[i])
+            print(a[last_noonzero_index])
+            a[last_noonzero_index], a[i] = a[i], a[last_noonzero_index]
+            last_noonzero_index += 1
+    return a
 
-movezero(arr)
+print(movezero(arr))
